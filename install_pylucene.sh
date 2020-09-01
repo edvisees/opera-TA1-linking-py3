aa084571ffc4c776d24b84612ae72d6c8598fa18
aditi <aditiasac@gmail.com>
2020-09-01 12:41:25 -0400

    install pylucene

--- install_pylucene.sh
+++ install_pylucene.sh
@@ -1,30 +1,14 @@
 #!/bin/bash -x
-
-cd $(dirname $0)
-
-export JAVA_HOME=${JAVA_HOME:-"/usr/lib/jvm/java-8-openjdk-amd64"}
-[ -e "$JAVA_HOME" ] || { echo "ERROR: Please set JAVA_HOME correctly!"; exit 1; }
-
-conda create -n xy_linking python=2.7.15
-source activate xy_linking
-
-if [ -d "../../resources/xianyang/xianyang_linking/pylucene-7.6.0" ]; then
-    cd ../../resources/xianyang/xianyang_linking/pylucene-7.6.0/jcc/
-else
-    # wget -q https://www-us.apache.org/dist/lucene/pylucene/pylucene-7.6.0-src.tar.gz
-    # tar xzf pylucene-7.6.0-src.tar.gz
-    # cd pylucene-7.6.0/jcc/
-    wget -q https://www-us.apache.org/dist/lucene/pylucene/pylucene-7.7.1-src.tar.gz
-    tar xzf pylucene-7.7.1-src.tar.gz
-    cd pylucene-7.7.1/jcc/
-fi
-export JCC_JDK=$JAVA_HOME
-export LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64:$JAVA_HOME/jre/lib/amd64/server:$JAVA_HOME/jre/lib:$LD_LIBRARY_PATH
-python setup.py clean && python setup.py build && python setup.py install
-cd ..
-make clean all install \
-    PYTHON=$(which python) \
-    JCC="$(which python) -m jcc --shared" \
-    ANT="JAVA_HOME=$JAVA_HOME /usr/bin/ant" \
-    NUM_FILES=8
-source deactivate
+# create a conda env called linking which contains pylucene with python 3
+conda create -n linking python=3.5
+source activate linking
+wget http://mirror.cc.columbia.edu/pub/software/apache/lucene/pylucene/pylucene-8.3.0-src.tar.gz
+tar xzf pylucene-8.3.0-src.tar.gz
+cd pylucene-8.3.0
+pushd jcc
+python setup.py build
+python setup.py install
+popd
+make
+make install
+source deactivate linking
